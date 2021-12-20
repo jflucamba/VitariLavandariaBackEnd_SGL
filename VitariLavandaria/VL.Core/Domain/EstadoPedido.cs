dabbba9d2688ebae9a8717a9a43480be8f848918
jflucamba <jflucamba@gmail.com>
2021-12-20 14:51:06 +0100

    alteração do campo Nome da tabela EstadoPedido.

--- VitariLavandaria/VL.Core/Domain/EstadoPedido.cs
+++ VitariLavandaria/VL.Core/Domain/EstadoPedido.cs
@@ -3,6 +3,6 @@
     public class EstadoPedido
     {
         public int Id { get; set; }
-        public string Nome { get; set; }
+        public string Descricao { get; set; }
     }
 }
---
+++ VitariLavandaria/VL.Data/Migrations/20211220134728_alterar-campo-tb-estadoPedido.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace VL.Data.Migrations
+{
+    public partial class alterarcampotbestadoPedido : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Nome",
+                table: "EstadoPedidos",
+                newName: "Descricao");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Descricao",
+                table: "EstadoPedidos",
+                newName: "Nome");
+        }
+    }
+}
