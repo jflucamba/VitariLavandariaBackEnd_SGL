1d46d23aa74e7b483cfd27d5125dc0c313809810
jflucamba <jflucamba@gmail.com>
2021-12-22 12:06:17 +0100

    criação do serviço gerardor de Token.

--- VitariLavandaria/VL.Manager/Interfaces/Manager/IUsuarioManager.cs
+++ VitariLavandaria/VL.Manager/Interfaces/Manager/IUsuarioManager.cs
@@ -1,6 +1,22 @@
-﻿namespace VL.Manager.Interfaces.Manager
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using VL.Core.Domain;
+using VL.Core.Shared.ModelView.Usuario;
+
+namespace VL.Manager.Interfaces.Manager
 {
     public interface IUsuarioManager
     {
+
+        Task<IEnumerable<UsuarioView>> GetAsync();
+
+        Task<UsuarioView> GetAsync(string login);
+
+        Task<UsuarioView> InsertAsync(NovoUsuario usuario);
+
+        Task<UsuarioView> UpdateMedicoAsync(Usuario usuario);
+
+        Task<UsuarioLogado> ValidaUsuarioEGeraTokenAsync(Usuario usuario);
+
     }
 }
---
+++ VitariLavandaria/VL.Manager/Interfaces/Services/IJWTService.cs
@@ -0,0 +1,9 @@
+﻿using VL.Core.Domain;
+
+namespace VL.Manager.Interfaces.Services
+{
+    public interface IJWTService
+    {
+        string GerarToken(Usuario usuario);
+    }
+}
